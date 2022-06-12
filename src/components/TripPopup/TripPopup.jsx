8481f0ecf8ea303cb5f5ff030f76775d4e0b7380
VladimirBludov <vladimir.bloodov@gmail.com>
2022-06-12 16:34:13 +0300

    refactoring TripPopap

--- src/components/TripPopup/TripPopup.jsx
+++ src/components/TripPopup/TripPopup.jsx
@@ -1,31 +1,67 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import Moment from 'moment';
 import Button from 'components/Button';
 import Input from 'components/Input';
 import TripInfo from 'components/TripInfo';
-import PropTypes from 'prop-types';
 import { CloseBtn, Container, Form, Total, Value } from './TripPopup.styles';
 
+const ONE_DAY_IN_MS = 86400000;
+
 export default function TripPopup({ trip, onClose }) {
+  const [date, setDate] = useState('');
+  const [guests, setGuests] = useState(1);
   const { title, duration, level, price } = trip;
+
+  const totalPrice = price * guests;
+  const minDate = Moment(Date.now() + ONE_DAY_IN_MS).format('YYYY-MM-DD');
+
+  const resetForm = () => {
+    setDate('');
+    setGuests(1);
+  };
+
+  const handelSubmit = e => {
+    e.preventDefault();
+
+    const booking = {
+      date,
+      guests,
+      totalPrice,
+    };
+    console.log('Booking: ', booking);
+
+    resetForm();
+  };
 
   return (
     <Container>
       <CloseBtn type="button" onClick={() => onClose()}>
         ×
       </CloseBtn>
-      <Form autoComplete="off">
+      <Form autoComplete="off" onSubmit={handelSubmit}>
         <TripInfo title={title} duration={duration} level={level} />
-        <Input title="Date" name="date" type="date" required />
+        <Input
+          title="Date"
+          name="date"
+          min={minDate}
+          value={date}
+          onChange={e => setDate(e.target.value)}
+          type="date"
+          required
+        />
         <Input
           title="Number of guests"
           name="guests"
           type="number"
           min="1"
           max="10"
-          value="1"
+          value={guests}
+          onChange={e => setGuests(e.target.value)}
           required
         />
         <Total>
-          Total: <Value>{price}$</Value>
+          Total: <Value>{totalPrice}$</Value>
         </Total>
         <Button text="Book a trip" type="submit" />
       </Form>
